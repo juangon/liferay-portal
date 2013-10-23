7b6751056f58dda8d03734a6e03683ab07d90eb6
Juan G <juan.gonzalez@liferay.com>
2013-10-23 08:56:05 +0200

    LPS-39361 There is no need to override the method

--- portal-impl/src/com/liferay/portlet/documentlibrary/util/PDFProcessorImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/util/PDFProcessorImpl.java
@@ -223,19 +223,6 @@ public class PDFProcessorImpl
 	}
 
 	@Override
-	protected void deletePreviews(
-		long companyId, long groupId, long fileEntryId, long fileVersionId) {
-
-		try {
-			DLStoreUtil.deleteDirectory(
-				companyId, REPOSITORY_ID,
-				getPathSegment(groupId, fileEntryId, fileVersionId, true));
-		}
-		catch (Exception e) {
-		}
-	}
-
-	@Override
 	protected void doExportGeneratedFiles(
 			PortletDataContext portletDataContext, FileEntry fileEntry,
 			Element fileEntryElement)
