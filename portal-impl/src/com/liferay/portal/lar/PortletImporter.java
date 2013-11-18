b0cc0d2dde825fc1fea26df84de1a9d322812afa
Brian Chan <brian.chan@liferay.com>
2013-11-18 09:06:45 -0800

    LPS-41889 SF

--- portal-impl/src/com/liferay/portal/lar/PortletImporter.java
+++ portal-impl/src/com/liferay/portal/lar/PortletImporter.java
@@ -501,6 +501,10 @@ public class PortletImporter {
 		portletDataContext.setSourceUserPersonalSiteGroupId(
 			sourceUserPersonalSiteGroupId);
 
+		// Read asset categories, asset tags, comments, locks, and ratings
+		// entries to make them available to the data handlers through the
+		// context
+
 		Element portletElement = null;
 
 		try {
@@ -517,10 +521,6 @@ public class PortletImporter {
 		}
 
 		LayoutCache layoutCache = new LayoutCache();
-
-		// Read asset categories, asset tags, comments, locks, and ratings
-		// entries to make them available to the data handlers through the
-		// context
 
 		if (importPermissions) {
 			_permissionImporter.checkRoles(
