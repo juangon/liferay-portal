6d2a48f826ec8c7f0a171b380fa7d13abbf88712
Raymond Auge <raymond.auge@liferay.com>
2013-11-21 11:40:18 -0500

    LPS-40360 formatting

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -4487,6 +4487,7 @@ public class PortalImpl implements Portal {
 				if (doAsGroupId <= 0) {
 					HttpServletRequest originalRequest =
 						getOriginalServletRequest(request);
+
 					doAsGroupId = ParamUtil.getLong(
 						originalRequest, "doAsGroupId");
 				}
