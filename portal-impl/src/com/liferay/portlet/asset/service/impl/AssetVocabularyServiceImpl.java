d64441cabbcec6066357eae81af4ac5c91547e1d
Juan G <juan.gonzalez@liferay.com>
2013-12-26 15:30:23 +0100

    LPS-43116 Check permissions before adding default vocabulary (it may be already added)

--- portal-impl/src/com/liferay/portlet/asset/service/impl/AssetVocabularyServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/impl/AssetVocabularyServiceImpl.java
@@ -250,7 +250,10 @@ public class AssetVocabularyServiceImpl extends AssetVocabularyServiceBaseImpl {
 			total = getGroupVocabulariesCount(groupId);
 		}
 
-		if (addDefaultVocabulary && (total == 0)) {
+		if (addDefaultVocabulary && (total == 0) &&
+			AssetVocabularyPermission.contains(
+				getPermissionChecker(), groupId, 0, ActionKeys.VIEW)) {
+
 			vocabularies = new ArrayList<AssetVocabulary>();
 
 			vocabularies.add(
