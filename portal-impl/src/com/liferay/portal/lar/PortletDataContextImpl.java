566bc0944ba43cabc61d243246851112f7f8158f
Máté Thurzó <mate.thurzo@liferay.com>
2014-03-05 13:08:49 +0100

    LPS-44232 Catching the exception anyway, no need to define a whole new block

--- portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
+++ portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
@@ -2167,21 +2167,10 @@ public class PortletDataContextImpl implements PortletDataContext {
 			referenceElement.addAttribute(
 				"group-id", String.valueOf(stagedGroupedModel.getGroupId()));
 
-			Group group = null;
-
 			try {
-				group = GroupLocalServiceUtil.getGroup(
+				Group group = GroupLocalServiceUtil.getGroup(
 					stagedGroupedModel.getGroupId());
-			}
-			catch (Exception e) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Unable to find group " +
-							stagedGroupedModel.getGroupId());
-				}
-			}
 
-			if (group != null) {
 				long liveGroupId = group.getLiveGroupId();
 
 				if (group.isStagedRemotely()) {
@@ -2194,6 +2183,13 @@ public class PortletDataContextImpl implements PortletDataContext {
 
 				referenceElement.addAttribute(
 					"live-group-id", String.valueOf(liveGroupId));
+			}
+			catch (Exception e) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(
+						"Unable to find group " +
+							stagedGroupedModel.getGroupId());
+				}
 			}
 		}
 
