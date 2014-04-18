3a7e1204b75cb94406c2c0556a6be6f614afb86b
Juan Gonzalez <juangon@gmail.com>
2014-04-18 10:32:05 +0200

    Revert "LPS-44232 SF"
    This reverts commit 899030279de7b2de8ebfa1354edb14eed6f49495.

--- portal-impl/src/com/liferay/portlet/sitesadmin/lar/StagedGroupStagedModelDataHandler.java
+++ portal-impl/src/com/liferay/portlet/sitesadmin/lar/StagedGroupStagedModelDataHandler.java
@@ -16,6 +16,7 @@ package com.liferay.portlet.sitesadmin.lar;
 
 import com.liferay.portal.kernel.lar.BaseStagedModelDataHandler;
 import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.PortletDataException;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.model.Group;
@@ -50,7 +51,8 @@ public class StagedGroupStagedModelDataHandler
 
 	@Override
 	public void importMissingReference(
-		PortletDataContext portletDataContext, Element referenceElement) {
+			PortletDataContext portletDataContext, Element referenceElement)
+		throws PortletDataException {
 
 		Map<Long, Long> groupIds =
 			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
