aaabf30b16b169c770492454093687a1f40693b1
Juan Gonzalez <juangon@gmail.com>
2015-12-08 20:30:20 +0100

    LPS-60470

--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageLocalServiceImpl.java
@@ -95,7 +95,6 @@ import com.liferay.portlet.trash.util.TrashUtil;
 import com.liferay.util.SerializableUtil;
 
 import java.io.InputStream;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -105,7 +104,7 @@ import java.util.List;
 import javax.portlet.PortletPreferences;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
-
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import net.htmlparser.jericho.Source;
@@ -1873,7 +1872,15 @@ public class MBMessageLocalServiceImpl extends MBMessageLocalServiceBaseImpl {
 
 		HttpServletRequest request = serviceContext.getRequest();
 
-		if (request == null) {
+		ServletContext servletContext = null;
+		
+		try {
+			servletContext = request.getServletContext();
+		}
+		catch (Exception e) {
+		}
+		
+		if ((request == null) || (servletContext == null)) {
 			if (Validator.isNull(serviceContext.getLayoutFullURL())) {
 				return StringPool.BLANK;
 			}
