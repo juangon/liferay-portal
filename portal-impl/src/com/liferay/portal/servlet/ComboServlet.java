1df291aab2d1f810327da01c4d45f1fca026dd36
Juan Gonzalez <juangon@gmail.com>
2016-04-10 20:43:11 +0200

    LPS-63997 redirect with a 404 error and throw NSLE to prevent security scans from thinking server is down with internal server error

--- portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
@@ -14,7 +14,7 @@
 
 package com.liferay.portal.servlet;
 
-import com.liferay.portal.ModulePathSetException;
+import com.liferay.portal.NoSuchLayoutException;
 import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.cache.SingleVMPoolUtil;
 import com.liferay.portal.kernel.log.Log;
@@ -111,7 +111,13 @@ public class ComboServlet extends HttpServlet {
 		}
 
 		if (modulePathsSet.size() == 0) {
-			throw new ModulePathSetException("Modules paths set is empty");
+			PortalUtil.sendError(
+				HttpServletResponse.SC_NOT_FOUND,
+				new NoSuchLayoutException(
+					"Query string translates to an empty module paths set"),
+					request, response);
+
+			return;
 		}
 
 		String[] modulePaths = modulePathsSet.toArray(
